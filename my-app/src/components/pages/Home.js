fd4bc70f505f4ff5082e335638ec4577cc63ec75
Sawant <2282521@cognizant.com>
2023-07-06 17:17:26 +0530

    first-commit

---
+++ my-app/src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from '../../components/Hero';
+import { SliderData } from '../../data/SliderData';
+import InfoSection from '../../components/InfoSection';
+import { InfoData, InfoDataTwo } from '../../data/InfoData';
+
+function Home() {
+
+    return(
+    <>
+      <Hero slides={SliderData} />
+      <InfoSection {...InfoData} />
+      <InfoSection {...InfoDataTwo} reverse={true} />
+    </>
+    );
+}
+
+export default Home;
